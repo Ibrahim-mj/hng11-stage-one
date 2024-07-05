89731509d0c40d068720de2f83c9f57e7c37874d
Ibrahim <ibraheemabdulmojeed1@gmail.com>
2024-07-05 07:43:44 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -23,8 +23,6 @@ class GreetUser(View):
         latitude = response.get('lat')
 
         weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={Config.WEATHER_API_KEY}").json()
-        print(weather_data)
-        print(Config.WEATHER_API_KEY)
         location_data = {
             "client_ip": client_ip,
             "city": response.get("city"),
